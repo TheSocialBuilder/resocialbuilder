1bb2b658f77a1f874d3ae306cdd32e609e43a740
Brett Hammond <bretthammond@stubthumb.com>
2012-06-06 15:36:45 -0600

    added carrier wave init with rackspace settings

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider           => 'Rackspace',
+    :rackspace_username => 'bhammond',
+    :rackspace_api_key  => '850842e4a357f1a0f8bdc312c32922d7'
+  }
+  config.fog_directory = 'resb_mls_listings'
+  config.fog_host = "http://c15062414.r14.cf2.rackcdn.com"
+end

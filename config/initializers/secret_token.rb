61bd1498a15b41b2089f9fc1bd7ee755ce67090b
Brett Hammond <bretthammond@stubthumb.com>
2012-06-06 14:09:33 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Resocialbuilder::Application.config.secret_token = 'add3ce65135c3d3969a537c1bec31936f55ab131fc4d1179b93c46de7e7f25df3e412bcca2477327002d98fede8ded602a6c94f4f466d4f5d0f68e5ac805efb4'
